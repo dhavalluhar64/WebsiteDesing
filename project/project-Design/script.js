2d8c6ef74c66bd3105cf6494bb66df24157bdf65
dhavalluhar64 <dhavalluhar0@gmail.com>
2024-08-16 16:23:59 +0530

    Project Design

---
+++ project/project-Design/script.js
@@ -0,0 +1,26 @@
+// nav-link hide and show
+
+let navLinks = document.getElementById("navLinks");
+
+
+function showMenu() {
+
+    navLinks.style.right = '0';
+    // navLinks.style.display = "block";
+   
+   
+   
+}
+
+function hideMenu() {
+
+    navLinks.style.right = '-200px';
+    // navLinks.style.display = "none";
+    
+ 
+    
+}   
+
+
+
+
